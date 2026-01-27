32231829f71f6e4e429596b4ef329f46d781c56e
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-01-27 12:10:42 +0530

    Add files via upload

---
+++ loops - theory - task.py
@@ -0,0 +1,12 @@
+for i in range(1,6,1):
+    if i==3:
+        break
+    print(i)
+for i in range(1,6,1):
+    if i==3:
+        continue
+    print(i)
+for i in range(1,6,1):
+    if i==3:
+        pass
+    print(i)
